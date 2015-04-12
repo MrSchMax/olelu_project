16152dca86e454c91829e99fdc93ab70c831d6bc
Schmax <mr.schmax@gmail.com>
2015-04-12 14:45:00 +0300

    add addons and main routes

--- app/router.js
+++ app/router.js
@@ -6,4 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 export default Router.map(function() {
+  this.route('main');
+  this.route('tours');
+  this.route('trainings');
+  this.route('events');
+  this.route('about');
 });
